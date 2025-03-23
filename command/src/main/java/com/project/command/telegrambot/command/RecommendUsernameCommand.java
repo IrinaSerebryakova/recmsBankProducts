8825ae769ae0219c090d52605c12e3626fdbc087
IrinaSerebryakova <ia.serebr@gmail.com>
2025-03-23 18:32:45 +0300

    feat (telegramBot,statistics,management): Телеграм-бот, статистика и META-INF
    Телеграм-бот выдаёт рекомендации пользователю (ищет username в базе данных,
    если он существует - начинает обращаться к пользователю в формате Имя+Фамилия).
    По запросу GET /rule/stats - выдается статистика срабатывания правил рекомендаций (сохраняется в таблице stats).
    В ManagementController добавлены запросы:
    /management/info  - возвращает информацию в формате: "name": "<название сервиса>","version": "<версия сервиса из файла pom.xml>".
    /management/clear-caches - очищает кэш всех запросов в системе.

--- command/src/main/java/com/project/command/telegrambot/command/RecommendUsernameCommand.java
+++ command/src/main/java/com/project/command/telegrambot/command/RecommendUsernameCommand.java
@@ -28,6 +28,7 @@ public class RecommendUsernameCommand implements TelegramCommand {
             String text = "Новые продукты для вас: " + userDtoRepository.getFullNameOfTelegramUser(update) + "!";
             return new SendMessage(chatId(update), text);
         }else{
+
             String text = "К сожалению, " + userDtoRepository.getFullNameOfTelegramUser(update) + ", " +
                     "вы не являетесь клиентом банка Star! \n" +
                     "Становитесь нашим клиентом до конца этого месяца, " +
